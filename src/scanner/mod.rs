0564670867e8100e045e006b37ca5f3fef1cec0e
Dom Slee <domslee1@gmail.com>
2026-04-22 21:55:34 +1000

    Scope dead_code allow to non-Windows only

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -167,7 +167,7 @@ pub struct ScanProgress {
 const MAX_FALLBACK_DETAILS: usize = 256;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[allow(dead_code)]
+#[cfg_attr(not(target_os = "windows"), allow(dead_code))]
 pub enum ScanFallbackKind {
     AccessDeniedOpen,
     OtherOpen,
